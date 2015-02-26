6a016b7e5c6c678be5709e0f2c0bbc3770d14159
Francesco Negri <francesco.negri.98@outlook.com>
2015-02-26 22:42:46 +0100

    Note
    Aggiunte alcune cose di poco conto.
    NB: discorso pixel perfect collision per semitrasparenza.

--- js/game.js
+++ js/game.js
@@ -81,10 +81,18 @@ function preload() {
 function create(x, y) {
     clientsList = {};
 
+    gameInstance.phaserGame.stage.backgroundColor = '#1B7B0C';
     gameInstance.depthGroup = gameInstance.phaserGame.add.group();
 
     tree = new TestingTree(gameInstance, 70, 120, 'tree');
     tree2 = new TestingTree(gameInstance, 600, 310, 'tree');
+    tree3 = new TestingTree(gameInstance, 450, 200, 'tree');
+    tree4 = new TestingTree(gameInstance, 390, 32, 'tree');
+    tree5 = new TestingTree(gameInstance, 650, 64, 'tree');
+    tree6 = new TestingTree(gameInstance, 100, 300, 'tree');
+    tree7 = new TestingTree(gameInstance, 140, 20, 'tree');
+    tree8 = new TestingTree(gameInstance, 230, 200, 'tree');
+    tree9 = new TestingTree(gameInstance, 400, 280, 'tree');
     player = new Player(gameInstance, x, y, 'player', myId, eurecaServer, eurecaClient);
     depthSort(gameInstance);       
 
