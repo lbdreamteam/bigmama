088455f4f2bf769ca5feea5f5bb6c557c14fb5d9
Francesco Negri <francesco.negri.98@outlook.com>
2015-02-27 19:45:11 +0100

    fix

--- LB Library/Entities/CharactersSystem/BaseCharacter.js
+++ LB Library/Entities/CharactersSystem/BaseCharacter.js
@@ -1,65 +1,13 @@
-<<<<<<< HEAD
-﻿BaseCharacter = function (gameInstance, x, y, graph) {
-    BaseEntity.call(this, gameInstance, x, y, graph);
-
-    //Proprietà
-    this.isMoving = false;
-    this.currentTile = { x: (x + (this.gameInstance.movementGridSize / 2)) / this.gameInstance.movementGridSize, y: (y + (this.gameInstance.movementGridSize / 2)) / this.gameInstance.movementGridSize };
-
-    this.zDepth = 0.5;
-    this.gameInstance.depthGroup.add(this);
-}
-
-BaseCharacter.prototype = Object.create(BaseEntity.prototype);
-BaseCharacter.prototype.constructor = BaseCharacter;
-
-
-BaseCharacter.prototype.createTween = function (character, target, onStartFunction, onCompleteFunction, input, duration, ease, autoStart, delay, repeat, yoyo) {
-
-    //Definizione parametri opzionali
-    if (typeof onStartFunction === 'undefined' || !onStartFunction) { onStartFunction = function () { } }
-    if (typeof onCompleteFunction === 'undefined' || !onCompleteFunction) { onCompleteFunction = function () { } }
-    if (typeof input === 'undefined' || !input) { input = 'null' }
-    if (typeof duration === 'undefined') { duration = 1000; }
-    if (typeof ease === 'undefined') { ease = Phaser.Easing.Default; }
-    if (typeof autoStart === 'undefined') { autoStart = false; }
-    if (typeof delay === 'undefined') { delay = 0; }
-    if (typeof repeat === 'undefined') { repeat = 0; }
-    if (typeof yoyo === 'undefined') { yoyo = false; }
-
-    var tween = character.gameInstance.phaserGame.add.tween(character);
-
-    tween.to(
-        target,
-        duration,
-        ease,
-        autoStart,
-        delay,
-        repeat, 
-        yoyo
-    );
-
-    tween.onStart.add(function () {
-        //console.log('tween partito: ' + character.id);
-        character.isMoving = true;
-        onStartFunction(character, input);
-        depthSort(gameInstance, character, target);
-    });
-
-    tween.onComplete.add(function () {
-        onCompleteFunction(character);
-        character.isMoving = false;
-    });
-
-    tween.start();
-=======
-BaseCharacter = function (gameInstance, x, y, graph, displayedName, nameVisible) {
+﻿BaseCharacter = function (gameInstance, x, y, graph, displayedName, nameVisible) {
     BaseEntity.call(this, gameInstance, x, y, graph);
 
     //Proprietà
     this.isMoving = false;
     this.currentTile = { x: (x + (this.gameInstance.movementGridSize / 2)) / this.gameInstance.movementGridSize, y: (y + (this.gameInstance.movementGridSize / 2)) / this.gameInstance.movementGridSize };
     
+    this.zDepth = 0.5;
+    this.gameInstance.depthGroup.add(this);
+
     if (typeof displayedName === 'undefined') { }
     else {
         if (typeof nameVisible === 'undefined' || false) {this.nameVisible = false}
@@ -104,6 +52,7 @@ BaseCharacter.prototype.createTween = function (character, target, onStartFuncti
         console.log('tween partito: ' + character.id);
         character.isMoving = true;
         onStartFunction(character, input);
+        depthSort(gameInstance, character, target);
     });
 
     tween.onComplete.add(function () {
