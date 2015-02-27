c62e0641fc4d7a4064efb6886a1cd412c056203b
Francesco Negri <francesco.negri.98@outlook.com>
2015-02-27 20:49:39 +0100

    fix last commit
    fix

---
+++ LB Library/Entities/ObjectsSystem/WorldObjects/LBBaseStaticObject.js
@@ -0,0 +1,12 @@
+﻿BaseStaticObject = function (game, x, y, graph) {
+    BaseWorldObject.call(this, game, x, y, graph);
+
+    //Proprietà
+}
+
+BaseStaticObject.prototype = Object.create(BaseWorldObject.prototype);
+BaseStaticObject.prototype.constructor = BaseStaticObject;
+
+BaseStaticObject.prototype.update = function () {
+
+}
