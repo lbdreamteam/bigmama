55974bc0cdead52e94b4da31179dfefdfff73946
ApoTeneggi <jacopoteneggi@live.com>
2015-04-14 11:57:49 +0200

    PRIMO COMMIT
    Aggiornate le funzioni per fare gli update ad ogni ciclo del server,
    iniziato refactoring per LBServer

--- server.js
+++ server.js
@@ -5,13 +5,28 @@ var express = require('express')
 app.use(express.static(__dirname));
 
 /************ VARIABILI GLOBALI ************/
+LBServer = function () {
+    //var eurecaServer = require('eureca.io').EurecaServer;
+    this.serverInstance;
+}
+
+LBServer.prototype = Object.create(Object);
+LBServer.prototype.constructor = LBServer;
+
+LBServer.prototype.init = function () {
+    var eurecaServer = require('eureca.io').EurecaServer;
+    this.serverInstance = eurecaServer;
+}
+
 var clients = {},
     EurecaServer = require('eureca.io').EurecaServer,
     eurecaServer = new EurecaServer({ allow: ['createGame', 'updatePlayer', 'updateOtherPlayers', 'onOtherPlayerConnect', 'onOtherPlayerDisconnect', 'spawnOtherPlayers'] }),
     callsCounter = 0,
     calls = {},
     posTable = { oldPos: {}, nowPos: {} },
-    states = { counter: 0, times: {}, pos: {} };
+    states = { counter: 0, times: {}, pos: {} },
+    nowConnected = {},
+    nowUpdating = {},
     t = 0;
 
 eurecaServer.attach(server);
@@ -19,35 +34,33 @@ t = Date.now();
 
 var updateInterval = setInterval(function () {
     posTable.oldPos = JSON.parse(JSON.stringify(posTable.nowPos));
-    if (JSON.stringify(posTable.nowPos) != '{}') {
-        states.times[states.counter] = Date.now() - t;
-        states.pos[states.times[states.counter]] = JSON.stringify(posTable.nowPos);
-        states.counter++;
-    }    
-    for (var client in clients) {
-        clients[client].remote.updateOtherPlayers(posTable.nowPos);
+    for (var client in nowConnected) {
+        //notifica
+        for (var other in clients) if (other != client) clients[other].remote.onOtherPlayerConnect(client, posTable.oldPos[client], posTable.nowPos[client]);
+        //fa lo spawn
+        clients[client].remote.spawnOtherPlayers(posTable);
+        delete nowConnected[client];
     }
+    var updatingPos = getUpdatingPos();
+    for (var client in nowUpdating) clients[client].remote.updateOtherPlayers(updatingPos);
+    nowUpdating = JSON.parse(JSON.stringify(clients));
 }, 100);
+
+var getUpdatingPos = function () {
+    var updatingPos = {};
+    for (var client in nowUpdating) updatingPos[client] = posTable.nowPos[client];
+    return updatingPos;
+}
 
 /************ EURECA - ON CONNECT ************/
 eurecaServer.onConnect(function (conn) {
     console.log('New Client id=%s ', conn.id, conn.remoteAddress);
     var remote = eurecaServer.getClient(conn.id);
     clients[conn.id] = { id: conn.id, remote: remote, state: { x: 1, y: 1 } };
-    posTable.oldPos[conn.id] = clients[conn.id].state;
     posTable.nowPos[conn.id] = clients[conn.id].state;
     remote.createGame(conn.id, clients[conn.id].state.x, clients[conn.id].state.y);
-    remote.spawnOtherPlayers(posTable);
-    remote.spawnOtherPlayers(clients);
-    for (var client in clients) {
-        console.log(clients[client].state.x);
-        if (client != conn.id) {
-            console.log('Found');
-            if (clients[client].remote) {
-                clients[client].remote.onOtherPlayerConnect(conn.id, posTable.oldPos[conn.id], posTable.nowPos[conn.id]);
-            }
-        }
-    }
+    nowConnected[conn.id] = Date.now();
+    delete nowUpdating[conn.id];
 });
 
 /************ EURECA - ON DISCONNECT ************/
@@ -55,6 +68,7 @@ eurecaServer.onDisconnect(function (conn) {
     console.log('Client disconnected ', conn.id);
     var removeId = clients[conn.id].id;
     delete clients[conn.id];
+    //delete nowUpdating[conn.id];
     delete posTable.oldPos[conn.id];
     delete posTable.nowPos[conn.id];
     for (var client in clients) {
