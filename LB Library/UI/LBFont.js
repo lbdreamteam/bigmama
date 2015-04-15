8c6b1cc95ad9478767c96370eaf733520fc844b1
AlessandroMessori <allemex@libero.it>
2015-04-15 21:42:28 +0200

    prime prove con l'xml parser

--- LB Library/UI/LBFont.js
+++ LB Library/UI/LBFont.js
@@ -1,1 +1,11 @@
-﻿
+﻿xml = new XMLHttpRequest;
+xml.open("GET", "assets/font.xml", false);
+xml.send();
+xmlDoc = xml.responseXML;
+
+//getElementsByTagName è un array che contiene tutte le tag del tipo specificato nel parametro nel file xml
+//childNodes[0].nodeValue è la propietà della tag che contiene il suo valore in stringa
+
+    console.log("");
+    console.log("Autore: " + xmlDoc.getElementsByTagName("chars")[0].childNodes[0].nodeValue);
+
