8df282aadaa29f7b597021a70966dbb898f89d8a
AlessandroMessori <allemex@libero.it>
2015-04-15 21:59:05 +0200

    Update
    aggiunte le funzioni che ritornano gli elementi degli attributi del file
    xml

--- LB Library/UI/LBFont.js
+++ LB Library/UI/LBFont.js
@@ -2,10 +2,17 @@
 xml.open("GET", "assets/font.xml", false);
 xml.send();
 xmlDoc = xml.responseXML;
+//mlDoc = loadXMLDoc("assets/font.xml");
 
 //getElementsByTagName è un array che contiene tutte le tag del tipo specificato nel parametro nel file xml
-//childNodes[0].nodeValue è la propietà della tag che contiene il suo valore in stringa
+//get attribute ritorna il valore dell attributo specificato nel parametro appartenente alla tag specificata da getElementsByTagName
 
-    console.log("");
-    console.log("Autore: " + xmlDoc.getElementsByTagName("chars")[0].childNodes[0].nodeValue);
+
+
+    console.log("Caratteristiche della lettera a del font:");
+    console.log("Id: " + xmlDoc.getElementsByTagName("char")[0].getAttribute('id'));
+    console.log("x: " + xmlDoc.getElementsByTagName("char")[0].getAttribute('x'));
+    console.log("y: " + xmlDoc.getElementsByTagName("char")[0].getAttribute('y'));
+    console.log("width: " + xmlDoc.getElementsByTagName("char")[0].getAttribute('width'));
+    console.log("height: " + xmlDoc.getElementsByTagName("char")[0].getAttribute('height'));
 
