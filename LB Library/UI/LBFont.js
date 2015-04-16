1c58e09aac498204b759437edc44552823b01198
Riccardo Spadaccini <richi.sp29@gmail.com>
2015-04-16 13:53:48 +0200

    Definita struttura classe Font
    Impostate proprietà ricavate da font.xml, //TODO: creare funzione per
    ritaglio immagine lettera e associare l'immagine al carattere

--- LB Library/UI/LBFont.js
+++ LB Library/UI/LBFont.js
@@ -1,18 +1,49 @@
-﻿xml = new XMLHttpRequest;
-xml.open("GET", "assets/font.xml", false);
-xml.send();
-xmlDoc = xml.responseXML;
-//mlDoc = loadXMLDoc("assets/font.xml");
+﻿LBFont = function (scale, italic, bold) {
+    if (scale == undefined || ( scale != "h1" && scale != "h2" && scale != "h3")) { scale = "h1"; }
+    if (italic == undefined) { italic = false; }
+    if (bold == undefined) { bold = false; }
 
-//getElementsByTagName è un array che contiene tutte le tag del tipo specificato nel parametro nel file xml
-//get attribute ritorna il valore dell attributo specificato nel parametro appartenente alla tag specificata da getElementsByTagName
+    this.scale = scale; //Grandezza del carattere (h1, h2, h3)
+    this.italic = italic;
+    this.bold = bold;
 
+    this.char = []; //Array dei caratteri ascii
+    this.width = []; 
+    this.height = []; 
+    this.x = [];
+    this.y = [];
 
+    //TODO: non va da solo il create
+    this.create();
+}
 
-    console.log("Caratteristiche della lettera a del font:");
-    console.log("Id: " + xmlDoc.getElementsByTagName("char")[0].getAttribute('id'));
-    console.log("x: " + xmlDoc.getElementsByTagName("char")[0].getAttribute('x'));
-    console.log("y: " + xmlDoc.getElementsByTagName("char")[0].getAttribute('y'));
-    console.log("width: " + xmlDoc.getElementsByTagName("char")[0].getAttribute('width'));
-    console.log("height: " + xmlDoc.getElementsByTagName("char")[0].getAttribute('height'));
+LBFont.prototype = Object.create(Object);
+LBFont.prototype.constructor = LBFont;
 
+LBFont.prototype.create = function () {
+    
+    //Set degli array
+    LBFont.prototype.fillArray(this.char, "id");
+    LBFont.prototype.fillArray(this.width, "width");
+    LBFont.prototype.fillArray(this.height, "height");
+    LBFont.prototype.fillArray(this.x, "x");
+    LBFont.prototype.fillArray(this.y, "y");
+
+}
+
+LBFont.prototype.fillArray = function (arr, attribute) {
+    //Viene caricato il documento XML del font
+    xml = new XMLHttpRequest;
+    xml.open("GET", "assets/font.xml", false);
+    xml.send();
+    xmlDoc = xml.responseXML;
+
+    count = xmlDoc.getElementsByTagName('chars')[0].getAttribute('count');
+
+    for (i = 0; i <= count; i++)
+    {
+        arr[i] = xmlDoc.getElementsByTagName('char')[i].getAttribute(attribute);
+        //console.log(arr[i]);
+    }
+}
+//TODO: creare funzione per ritaglio immagine lettera e associare l'immagine al carattere
