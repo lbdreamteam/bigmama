bce7249614223f94fb25d875aec52365a3da473c
AlessandroMessori <allemex@libero.it>
2015-04-27 15:51:06 +0200

    Update
    aggiunta la possibilità di displeyare le singole lettere del font

--- LB Library/UI/LBFont.js
+++ LB Library/UI/LBFont.js
@@ -1,18 +1,21 @@
 ﻿LBFont = function (scale, italic, bold) {
-    if (scale == undefined || ( scale != "h1" && scale != "h2" && scale != "h3")) { scale = "h1"; }
+    if (scale == undefined || (scale != "h1" && scale != "h2" && scale != "h3")) { scale = "h1"; }
     if (italic == undefined) { italic = false; }
     if (bold == undefined) { bold = false; }
+
+
 
     this.scale = scale; //Grandezza del carattere (h1, h2, h3)
     this.italic = italic;
     this.bold = bold;
 
     this.char = []; //Array dei caratteri ascii
-    this.fwidth = []; 
-    this.fheight = []; 
+    this.fwidth = [];
+    this.fheight = [];
     this.fx = [];
     this.fy = [];
     this.single_char = [];
+    this.rect = [];
 
     this.create();
 }
@@ -21,7 +24,7 @@ LBFont.prototype = Object.create(Object);
 LBFont.prototype.constructor = LBFont;
 
 LBFont.prototype.create = function () {
-    
+
     //Set degli array
     this.fillArray(this.char, "id");
     this.fillArray(this.fwidth, "width");
@@ -30,8 +33,6 @@ LBFont.prototype.create = function () {
     this.fillArray(this.fy, "y");
     this.imageHandler();
 }
-
-
 
 LBFont.prototype.fillArray = function (arr, attribute) {
     //Viene caricato il documento XML del font
@@ -42,10 +43,9 @@ LBFont.prototype.fillArray = function (arr, attribute) {
 
     count = xmlDoc.getElementsByTagName('chars')[0].getAttribute('count');
 
-    for (i = 0; i <= count; i++)
-    {
+    for (i = 0; i <= count; i++) {
         arr[i] = xmlDoc.getElementsByTagName('char')[i].getAttribute(attribute);
-       
+
     }
 }
 
@@ -53,12 +53,24 @@ LBFont.prototype.imageHandler = function () {
 
     console.log('imageHandler partito');
 
-    for (var i = 0; i < this.char.length; i++) {
-        var im = gameInstance.phaserGame.cache.getImage('font_table');
-        var rect = new Phaser.Rectangle(this.fx[i], this.fy[i], this.fwidth[i], this.fheight[i]);
-        var bm = new Phaser.BitmapData(gameInstance.phaserGame, 'bitmap_font', im.width, im.height);
-        bm.draw(im);
-        bm.update();
-        this.single_char[i] = bm.getPixels(rect);
+
+    var im = gameInstance.phaserGame.cache.getImage('font_table');
+
+    var bmd = gameInstance.phaserGame.add.bitmapData(gameInstance.width, gameInstance.height);
+
+    // funzioni da gestire con il depth group
+    // bmd.addToWorld();
+
+    //bdm.draw(im, 0, 0);
+
+    for (var i = 0; i < this.char.length - 1; i++) {
+
+        this.rect[i] = new Phaser.Rectangle(this.fx[i], this.fy[i], this.fwidth[i], this.fheight[i]);
+        this.single_char[i] = bmd.getPixels(this.rect[i]).data;
+
     }
+
+    //funzione che mostra la lettera a 
+    // bmd.ctx.putImageData(this.rect[0], x, y);
 }
+
