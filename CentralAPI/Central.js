5ea8ac180f771416ac86dd80672870aa669bfc44
ApoTeneggi <jacopoteneggi@live.com>
2015-04-28 23:24:48 +0200

    UPDATE
    Nuova versione central:
    -inserita estrazione della porta da db,
    -scritta parte comandi bash (da debuggare)

--- CentralAPI/Central.js
+++ CentralAPI/Central.js
@@ -21,7 +21,7 @@ router.use(function (req, res, next) {
 });
 
 router.get('/loadPorts/:startPort/:max/:pullSize', function (req, res) {
-    var lastPort = req.params.startPort;
+    var lastPort = parseInt(req.params.startPort);
     for (var i = 0; i < req.params.max/req.params.pullSize; i++) {
         var portPull = []
         for (var j = 0; j < req.params.pullSize; j++) {
@@ -52,8 +52,48 @@ router.get('/redirect/:port', function (req, res) {
 });
 
 router.get('/create', function (req, res) {
-
-})
+    var index = Math.floor(Math.random() * 10);
+    dynDB.getItem({
+        'AttributesToGet' : ['pull'],
+        'Key': {
+            'index': {
+                'N': index.toString()
+            }
+        },
+        'TableName': 'ports'
+    }, function (err, data) {
+        if (err) res.json({ err: err })
+        else {
+            var port = data.Item.pull.NS[Math.floor(Math.random() * data.Item.pull.NS.length)];
+            console.log('pulllength : ' + data.Item.pull.NS.length + ' port: ' + port);
+            dynDB.updateItem({
+                'UpdateExpression': 'DELETE pull :port',
+                'ExpressionAttributeValues' : {
+                    ':port' : {
+                        'NS' : [port.toString()]
+                    }
+                },
+                'Key': {
+                    'index': {
+                        'N': index.toString()
+                    }
+                },
+                'TableName' : 'ports',
+                'ReturnValues': 'UPDATED_NEW'
+            }, function (err, data) {
+                if (err) res.json({ err: err })
+                else {
+                    var child;
+                    child = exec('node server.js ' + port);
+                    console.log('Started game on port: ' + port);
+                    child = exec('aws ec2 authorize-security-group-ingress --group-id sg-0787d562 --protocol tcp --port ' + port + ' --cidr 0.0.0.0/0 --region eu-west-1');
+                    console.log('Opened port: ' + port);
+                    res.redirect('http://52.17.92.120:' + port);
+                }
+            });
+        }
+    });
+});
 
 app.use('/api', router);
 
