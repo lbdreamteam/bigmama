8edd592c9f35d4a3fa3013320f9d65d609193acf
ApoTeneggi <jacopoteneggi@live.com>
2015-04-29 23:42:02 +0200

    UPDATE
    -aggiunta chiusura porta con reinserimento nel DB

--- CentralAPI/Central.js
+++ CentralAPI/Central.js
@@ -52,12 +52,12 @@ router.get('/redirect/:port', function (req, res) {
 });
 
 router.get('/create', function (req, res) {
-    var index = Math.floor(Math.random() * 10);
+    var pullIndex = Math.floor(Math.random() * 10);
     dynDB.getItem({
         'AttributesToGet' : ['pull'],
         'Key': {
             'index': {
-                'N': index.toString()
+                'N': pullIndex.toString()
             }
         },
         'TableName': 'ports'
@@ -75,7 +75,7 @@ router.get('/create', function (req, res) {
                 },
                 'Key': {
                     'index': {
-                        'N': index.toString()
+                        'N': pullIndex.toString()
                     }
                 },
                 'TableName' : 'ports',
@@ -94,6 +94,78 @@ router.get('/create', function (req, res) {
         }
     });
 });
+
+router.get('/kill/:port', function (req, res) {
+    var port = req.params.port;
+    dynDB.scan({
+        'TableName': 'ports',
+        'ProjectionExpression': '#index',
+        'FilterExpression': 'size (pull) < :val',
+        'ExpressionAttributeNames' : {
+            '#index': 'index'
+        },
+        'ExpressionAttributeValues': {
+            ':val': {
+                'N' : '5'
+            }
+        }
+    }, function (err, data) {
+        if (err) res.json({ err: err })
+        else {
+            if (!data.Count) res.json({ err: 'ALERT! SEEMS TO BE NO GAME TO CLOSE!' })
+            else {
+                var pullIndex = data.Items[Math.floor(Math.random() * (data.Count - 1))].index.N;
+                dynDB.updateItem({
+                    'UpdateExpression': 'ADD pull :port',
+                    'ConditionExpression': 'NOT contains ( pull, :port )',
+                    'ExpressionAttributeValues': {
+                        ':port': {
+                            'NS': [port.toString()]
+                        }
+                    },
+                    'Key': {
+                        'index': {
+                            'N': pullIndex.toString()
+                        }
+                    },
+                    'TableName': 'ports',
+                    'ReturnValues': 'UPDATED_NEW'
+                }, function (err, data) {
+                    if (err) res.json({ err: err })
+                    else {
+                        var child;
+                        child = exec('fuser -k ' + port + '/tcp');
+                        console.log('Terminated LBGame on port: ' + port);
+                        child = exec('aws ec2 revoke-security-group-ingress --group-id sg-0787d562 --protocol tcp --port ' + port + ' --cidr 0.0.0.0/0 --region eu-west-1');
+                        console.log('Closed port: ' + port);
+                        res.json({ response: true });
+                    }
+                });
+            }
+        }
+    });
+});
+
+//router.get('/test', function (req, res) {
+//    dynDB.updateItem({
+//        'UpdateExpression': 'ADD pull :port',
+//        'ConditionExpression': 'NOT contains ( pull, :port)',
+//        'ExpressionAttributeValues': {
+//            ':port': {
+//                'NS': ['9013']
+//            }
+//        },
+//        'Key': {
+//            'index': {
+//                'N': '2'
+//            }
+//        },
+//        'TableName': 'ports'
+//    }, function (err, data) {
+//        if (err) res.json({ err: err })
+//        else res.json({ response: data });
+//    });
+//});
 
 app.use('/api', router);
 
