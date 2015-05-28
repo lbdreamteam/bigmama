a277afaa6317b2e825571e17a2e1d7f30cc9cbad
Riccardo Spadaccini <richi.sp29@gmail.com>
2015-05-28 13:54:36 +0200

    Update
    iniziata classe LBButton

---
+++ LB Library/UI/LBButton.js
@@ -0,0 +1,20 @@
+﻿LBButton = function (style, text, x, y, callback) {
+
+    this.b_style = style;
+    this.b_text = text;
+    this.b_x = x;
+    this.b_y = y;
+    this.b_callback = callback;
+    this.b_state = 'out';
+}
+
+LBButton.prototype = Object.create(Object);
+LBButton.prototype.constructor = LBButton;
+
+LBButton.prototype.create = function () {
+
+}
+
+LBButton.prototype.eventHandler = function () {
+    
+}
